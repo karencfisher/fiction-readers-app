5eda8e514c8c5dd75c92c4cd76ae9f780156dd96
karencfisher <karen@karencfisher.com>
2024-11-13 10:43:19 -0700

    models and first view

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,6 +1,58 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Books(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    year_published = models.IntegerField()
+    title = models.CharField(max_length=100)
+    synopsis = models.TextField()
+    info_link = models.CharField(max_length=100)
+    average_rating = models.IntegerField(default=0)
+    cover_link= models.CharField(max_length=100)
+    author = models.ForeignKey('Authors', on_delete=models.CASCADE, null=True)
+    publisher = models.ForeignKey('Publishers', on_delete=models.CASCADE, null=True)
+    genre = models.ForeignKey('Genres', on_delete=models.CASCADE, null=True)
+    reviews = models.ManyToManyField('Reviews', related_name='books')
+    
+    
+class Authors(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    author_name= models.TextField()
+    
+    def books(self):
+        return Books.objects.filter(author=self)
+    
 
+class Publishers(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    publisher_name = models.TextField()
+    
+    def books(self):
+        return Books.objects.filter(publisher=self)
+    
+    
+class Genres(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    genre = models.TextField()
+    
+    def books(self):
+        return Books.objects.filter(genre=self)
+    
+    
+class Reviews(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    
+    
+class ReaderLogs(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    status = models.TextChoices('status', 'TOREAD READING DNF SKIMMED COVER2COVER')
+    book = models.ManyToManyField('Books', related_name='reader_logs')
+    
+    
--- _server/core/views.py
+++ _server/core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.conf  import settings
 import json
 import os
+import traceback
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.conf  import settings
 from django.contrib.auth.decorators import login_required
+from .models import Books, Authors, Genres, Publishers
 
 # Load manifest when server launches
 MANIFEST = {}
@@ -21,3 +25,30 @@ def index(req):
         "css_file": "" if settings.DEBUG else MANIFEST["src/main.ts"]["css"][0]
     }
     return render(req, "core/index.html", context)
+
+# @login_required
+def search(req):
+    try:
+        method = req.GET['method']
+        query = req.GET['query']
+        if method == 'author':
+            collection = Authors.objects.get(author_name=query)
+        elif method == 'genre':
+            collection = Genres.objects.get(genre=query)
+        elif method == 'publisher':
+            collection = Publishers.objects.get(publisher_name=query)
+        elif method == 'title':
+            pass
+        elif method == 'synopsis':
+            pass
+        else:
+            raise KeyError(f'No such query method: {query}')
+        return JsonResponse(model_to_dict(collection.books()))
+    except (Genres.DoesNotExist, 
+            Authors.DoesNotExist, 
+            Publishers.DoesNotExist):
+        return JsonResponse({'error': f"{method} '{query}' is not found"}, status=404)
+    except Exception as err:
+        traceback.print_tb(err.__traceback__)
+        print(err)
+        return JsonResponse({'error': 'Invalid request'}, status=400)
