77515562f76d1afd6a13dd4691a8f14f3795786b
karencfisher <karen@karencfisher.com>
2025-04-28 17:40:28 -0600

    add screenshots directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Fetch CSRF token on app initialization
+    fetch('/registration/csrf/', {
+      credentials: 'same-origin',
+    });
+  }, []);
+  
   return (
     <DndProvider backend={HTML5Backend}>
       <Outlet />
