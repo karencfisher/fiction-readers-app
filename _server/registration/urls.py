036da3e397db4ccae92141acee2e7a3d814b27f9
karencfisher <karen@karencfisher.com>
2025-04-28 17:44:45 -0600

    fixed CSRF issue

--- _server/registration/urls.py
+++ _server/registration/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('csrf/', views.get_csrf),
     path('sign_in/', views.sign_in),
     path('sign_up/', views.sign_up),
     path('logout/', views.logout_view),
